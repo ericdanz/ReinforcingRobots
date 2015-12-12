dabebf8509e7502d96362ec3ff8636888f0e2197
Eric <eric@cogniac.co>
2015-12-12 13:00:36 -0800

    fixing time delay reward propagation

--- make_states.py
+++ make_states.py
@@ -4,14 +4,16 @@ from simulation_simulator import Simulator
 
 
 
-def make_states(simulator,actor,epsilon,number_of_steps,number_of_games):
+def make_states(simulator,actor,epsilon,number_of_steps,number_of_games,winners_only=False):
     #initialize something to hold the games
     #assume epsilon decay happens outside this function
     game_list = []
     for i in range(number_of_games):
+        simulator.reset(64,10)
         state_list = make_one_set(simulator,actor,epsilon,number_of_steps)
-        for state in state_list:
-            game_list.append(state)
+        if (len(state_list) < number_of_steps) or (not winners_only):
+            for state in state_list:
+                game_list.append(state)
     return game_list
 
 
@@ -34,21 +36,24 @@ def make_one_set(simulator,actor,epsilon,number_of_steps):
             screen,score,end = simulator.do_action(action)
         if end == None:
             screen,score,end = simulator.do_action(action)
+
+        state.append([screen,score,action])
+        state_list.append(copy.copy(state))
+
         #check if the actor won!
         if end:
             #propogate the reward back
             reward = score
-            discount_iterator = 1
+            discount_iterator = 0
             discount_factor = 0.1
-            for state in reversed(state_list):
+            for previous_state in reversed(state_list):
                 #this starts at the state before the win
                 #the reward can be decreased linearly or exponentially
                 #this will do it linearly
-                state[0][1] += reward*(1- (discount_factor*discount_iterator))
+                previous_state[0][1] += reward*(1- (discount_factor*discount_iterator))
                 discount_iterator += 1
 
-        state.append([screen,score,action])
-        state_list.append(copy.copy(state))
+            break
 
     return state_list
 
@@ -62,10 +67,11 @@ class FakeActor:
 
 
 if __name__ == "__main__":
-    sim = Simulator(256,10)
+    sim = Simulator(64,10)
     actor = FakeActor(4)
-    game_state_list = make_states(sim,actor,1,10,10)
+    game_state_list = make_states(sim,actor,1,10,20,winners_only=False)
     for state_list in game_state_list:
         for state in state_list:
-            cv2.imshow('sim',state[0][0])
+            print(state[1])
+            cv2.imshow('sim',state[0])
             cv2.waitKey(1000)
